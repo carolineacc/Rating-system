c4483c51a9919e93969dc26c2e304d0bd31b3e73
carolineacc <carolinejf14@gmail.com>
2026-02-05 18:09:10 +0800

    Add files via upload
    English version update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,17 +7,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
-import zhCN from 'antd/locale/zh_CN';
-import 'dayjs/locale/zh-cn';
+import enUS from 'antd/locale/en_US';
+import 'dayjs/locale/en';
 import App from './App';
 
-// 渲染React应用到页面的#root元素
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* BrowserRouter: 提供路由功能 */}
     <BrowserRouter>
-      {/* ConfigProvider: 配置Ant Design为中文 */}
-      <ConfigProvider locale={zhCN}>
+      <ConfigProvider locale={enUS}>
         <App />
       </ConfigProvider>
     </BrowserRouter>
